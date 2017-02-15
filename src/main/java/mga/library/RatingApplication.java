7e413a63791f24b1df35f1d371b88b2464f19ef3
Maria Gomez <mgomez@thoughtworks.com>
2017-02-15 18:41:54 -0500

    7 Created rating microservice

---
+++ src/main/java/mga/library/RatingApplication.java
@@ -0,0 +1,12 @@
+package mga.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingApplication.class, args);
+	}
+}
---
+++ src/test/java/mga/library/RatingApplicationTests.java
@@ -0,0 +1,16 @@
+package mga.library;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RatingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
